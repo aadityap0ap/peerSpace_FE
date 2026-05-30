5472065ccefeb16e80fd66dd750811bec9697695
aadityap0ap <adityabharti450ap@gmail.com>
2026-05-30 17:37:44 +0530

    Extract message screen into separate MessagePage component from App Component

--- peerspacefe/src/App.tsx
+++ peerspacefe/src/App.tsx
@@ -1,17 +1,9 @@
-export default function App() {
-  return (
-    <div className="h-screen bg-black flex flex-col">
-      <div className="flex-1"></div>
-      <div className="p-4 bg-gray-900 flex gap-2">
-        <input
-          type="text"
-          placeholder="Type a message..."
-          className="flex-1 p-3 rounded bg-gray-800 text-white outline-none"
-        />
-        <button className="bg-purple-600 px-5 py-3 rounded text-white">
-          Send
-        </button>
-      </div>
+import MessagePage from "./MessagePage";
+
+export default function App(){
+  return(
+    <div>
+      <MessagePage/>
     </div>
-  );
+  )
 }
---
+++ peerspacefe/src/MessagePage.tsx
@@ -0,0 +1,17 @@
+export default function MessagePage() {
+  return (
+    <div className="h-screen bg-black flex flex-col">
+      <div className="flex-1"></div>
+      <div className="p-4 bg-gray-900 flex gap-2">
+        <input
+          type="text"
+          placeholder="Type a message..."
+          className="flex-1 p-3 rounded bg-gray-800 text-white outline-none"
+        />
+        <button className="bg-purple-600 px-5 py-3 rounded text-white">
+          Send
+        </button>
+      </div>
+    </div>
+  );
+}
