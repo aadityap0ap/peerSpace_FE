7222645b707758735146b4c6d2cbf74f6ae4a104
aadityap0ap <adityabharti450ap@gmail.com>
2026-06-03 18:51:25 +0530

    Implemented  client-side routing with React Router

--- peerspacefe/src/App.tsx
+++ peerspacefe/src/App.tsx
@@ -1,10 +1,17 @@
-//import { SignIn } from "./Pages/SignIn";
+import { BrowserRouter, Route,Routes } from "react-router-dom";
+import { SignIn } from "./Pages/SignIn";
+import MessagePage from "./Pages/MessagePage";
 import { SignUp } from "./Pages/SignUp";
-
+import HomePage from "./Pages/HomePage";
 export default function App(){
   return(
-    <div>
-      <SignUp/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/messagepage" element={<MessagePage/>}/>
+      <Route path="/homepage" element={<HomePage/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
