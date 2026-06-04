9244785831f9ce1329eb716a25bc7edabb0ba936
aadityap0ap <adityabharti450ap@gmail.com>
2026-06-04 19:15:49 +0530

    implemented routing from home page to auth pages

--- peerspacefe/src/Pages/Home.tsx
+++ peerspacefe/src/Pages/Home.tsx
@@ -1,17 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+
 export default function Home() {
+   const navigate = useNavigate();
     return (
         <div className="h-screen w-screen flex justify-center items-center bg-gray-100">
             <div className="border p-8 w-80 flex flex-col gap-4 rounded-lg shadow-lg bg-white">
                 
                 <p className="text-center">
                     New User?{" "}
-                    <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600">
+                    <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600"
+                    onClick={() => navigate("/SignUp")}>
                         Sign Up
                     </button>
                 </p>
 
                 <p className="text-center text-xl font-semibold">
-                    <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600">
+                    <button className="bg-blue-300 text-white px-3 py-1 rounded hover:bg-blue-600" 
+                    onClick={() => navigate("/SignIn")}>
                         Sign In
                     </button>
                 </p>
@@ -20,3 +26,4 @@ export default function Home() {
         </div>
     );
 }
+
