a4f857b67a9e289175dc00eda9803e4cdc80a3d1
aadityap0ap <adityabharti450ap@gmail.com>
2026-06-05 22:23:33 +0530

    Made Signup page full responsive and synced it with backend also

--- peerspacefe/src/Pages/SignUp.tsx
+++ peerspacefe/src/Pages/SignUp.tsx
@@ -1,4 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
+
+
 export function SignUp() {
+  const [email,setEmail] = useState("");
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+
+  async function handleSignUp(){
+    try{
+      const response =  await axios.post(
+        "http://localhost:3000/auth/signup",
+        {
+          email,
+          username,
+          password
+        }
+      );
+      alert(response.data.message);
+      setEmail("");
+      setUsername("");
+      setPassword("");
+
+    }
+    catch(error : any){
+      alert(error.response?.data?.message ||"SignUp failed");
+    }
+  }
   return (
     <div className="h-screen w-screen bg-gray-100 flex justify-center items-center">
       <div className="bg-white rounded-lg border p-8 w-80 flex flex-col">
@@ -6,19 +34,32 @@ export function SignUp() {
         <h2 className="text-2xl font-semibold text-center mb-6">
           Sign Up
         </h2>
+        
+         <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="px-4 py-2 border rounded mb-3 outline-none focus:border-blue-500"
+          placeholder="Email"
+        />
 
         <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           className="px-4 py-2 border rounded mb-3 outline-none focus:border-blue-500"
           placeholder="Username"
         />
 
         <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           type="password"
           className="px-4 py-2 border rounded mb-4 outline-none focus:border-blue-500"
           placeholder="Password"
         />
 
-        <button className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
+        <button
+         onClick={handleSignUp}
+         className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
           Sign Up
         </button>
 
